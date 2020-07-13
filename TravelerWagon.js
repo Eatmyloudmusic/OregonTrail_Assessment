b2cf3e84fa6c3252bae25a08f22be87b866c0d8d
william_michael_brough <eatmyloudmusic@gmail.com>
2020-07-13 10:22:41 -0400

    Last commit

--- TravelerWagon.js
+++ TravelerWagon.js
@@ -1,80 +1,77 @@
 // let totalFoodVar = 0
 class Traveler {
-    constructor(name, food, isHealthy) {
+    constructor(name) {
         this.name = name
         this.food = 1
         this.isHealthy = true
-        
-        
+
+
     }
-    hunt () {
+    hunt() {
         // Wagon.totalFood += 
         this.food += 2
-        console.log("We found food, Ya'll!")
+        console.log(this.name + " went out and found food, Ya'll! But you'll have to get your own I'm afraid.")
         // console.log(this.food)
     }
-    eat () {
+    eat() {
         // this.food -= 1
-        if (this.food === 0) {
-            this.isHealthy = false
+        if (this.food < 1) {
+            return this.isHealthy = false
             // console.log(this.isHealthy)
         } else {
             this.food -= 1
+            // return this.isHealthy = false
         }
+        // this.food -= 1
     }
 
 }
 class Wagon {
-    constructor(capacity, passengers) {
-        this.capacity = 2
-        this.passengers = []
+    constructor(seats) {
+        this.capacity = seats
+        this.groupOfPilgrims = []
     }
     //push traveller into .passengers
-    getAvailableSeatCount (){
-        let numberOfEmptySeats = this.capacity
-        return numberOfEmptySeats 
+    getAvailableSeatCount() {
+        return this.capacity - this.groupOfPilgrims.length
     }
-    join () {
-        // let traveler = []
+    join(defNotTheDonnerParty) {
+        if (this.getAvailableSeatCount() <= 0) {
+            console.log("Sorry, but the wagon's full, pilgrim. Best wait fer another.")
+            return "Full Up."
+        }
+        else {
+            this.groupOfPilgrims.push(defNotTheDonnerParty)
+        }
+        // let defNotTheDonnerParty = []
         // this.capacity -= 1
-        console.log(this.name)
-
-        if (this.capacity > 0) {
-            // this.capacity = 0
-            this.passengers.push(Traveler)
-            // console.log('passerger')
-            this.capacity -= 1
-            }
-
-            // console.log("Sorry, but the wagon's full, pilgrim. Best wait fer another.")
-// console.log(Traveler.name)
+        // console.log(this.name)
+        // console.log("Sorry, but the wagon's full, pilgrim. Best wait fer another.")
+        // console.log(defNotTheDonnerParty.name)
     }
-    shouldQuarantine () {
+    shouldQuarantine() {
+        const verySick = this.groupOfPilgrims.some(defNotTheDonnerParty => defNotTheDonnerParty.isHealthy === false)
         // for (let i of this.passengers) {
-            
-console.log(this.isHealthy)
-            if (this.isHealthy === false) {
-                return true
-            }
-        
-        return false
+        if (verySick === true) {
+            return true
+        }
+        else {
+            return false
+        }
+        // console.log(this.isHealthy)
+        //             if (this.isHealthy === false) {
+        //                 return true
     }
-    totalFood () {
-
-
+    totalFood() {
         // for(var i = 0; i < this.passengers.length; i++){
         //     console.log(this.passengers[i]);
         // }
         // console.log(this.food)
         let allFood = 0
-        for (this.food of this.passengers) {
-            allFood = allFood + this.food
-            
-
+        for (let index = 0; index < this.groupOfPilgrims.length; index += 1) {
+            allFood += this.groupOfPilgrims[index].food
         }
         return allFood
         // ::make array of passengers and their food. add food.
-        // let allFood = "array of food added together."
-        // return allFood
     }
 }
